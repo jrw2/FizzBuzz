4a7448c19a5a9403bd333d0d16b037007d31ce0e
James Westmoreland <w7jr@me.com>
2014-08-29 07:38:57 -0600

    Added code and tested. There is something wrong with the Swift solution but I can't see it.

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -15,7 +15,47 @@
     
     // Insert code here
     
+    for (int i = 1; i < 1000; i++) {
+        BOOL buzzed = isBuzzed(i);
+        BOOL fizzed = isFizzed(i);
+        if (fizzed && !buzzed)
+            NSLog(@"Fizz");
+        else if (!fizzed && buzzed)
+            NSLog(@"Buzz");
+        else if (fizzed && buzzed)
+            NSLog(@"FizzBuzz");
+        else
+            NSLog(@"%d", i);
+    }
+    
     return YES;
+}
+
+BOOL isBuzzed(int number) {
+    return checkFizzBuzz(number, 3);
+}
+
+BOOL isFizzed(int number) {
+    return checkFizzBuzz(number, 5);
+}
+
+BOOL checkFizzBuzz(int number, int checkNumber) {
+    BOOL status = false;
+    
+    if ((number % checkNumber) == 0)
+        status = true;
+    else {
+        int n = number;
+        for (int i = 0; i < 3; i++) {
+            if ((n % 10) == checkNumber) {
+                status = true;
+                break;
+            }
+            n /= 10;
+        }
+    }
+    
+    return status;
 }
 
 @end
--- FizzBuzz-Swift/FizzBuzz-Swift/AppDelegate.swift
+++ FizzBuzz-Swift/FizzBuzz-Swift/AppDelegate.swift
@@ -18,9 +18,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Insert code here...
         
-        
+        for i in 1..<1000 {
+            var buzzed: Bool = isBuzzed(i)
+            var fizzed: Bool = isFizzed(i)
+            if (fizzed && !buzzed) {
+                NSLog("Fizz")
+            } else if (!fizzed && buzzed) {
+                NSLog("Buzz")
+            } else if (fizzed && buzzed) {
+                NSLog("FizzBuzz")
+            } else {
+                NSLog("\(i)")
+            }
+        }
         
         return true
+    }
+    
+    func isBuzzed(number: Int) -> Bool {
+        return checkFizzBuzz(number, 3)
+    }
+    
+    func isFizzed(number: Int) -> Bool {
+        return checkFizzBuzz(number, 5)
+    }
+    
+    func checkFizzBuzz(number: Int, checkNumber: Int) -> Bool {
+        var status: Bool = false
+    
+        if ((number % checkNumber) == 0) {
+            status = true
+        } else {
+            var n: Int = number
+            for i in 0..<3 {
+                if ((n % 10) == checkNumber) {
+                    status = true
+                    break
+                }
+                n /= 10
+            }
+        }
+    
+    return status
     }
 
  }
